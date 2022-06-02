be81702b47d17548588700ee5ec7717b4eb2b007
mturczak <mturczak@users.noreply.github.com>
2022-06-02 15:28:44 +0200

    final commit, error modal, adding users on a list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UsersList";
 
 function App() {
+   const [usersList, setUsersList] = useState([]);
+
+   const onAddUserHandler = (uName, uAge) => {
+     setUsersList((prevUsersList) => {
+       return [...prevUsersList, {name: uName, age: uAge, id:Math.random().toString()}];
+     });
+   }
   return <div>
-    <AddUser />
-    <UserList users={[]} />
+    <AddUser onAddUser={onAddUserHandler} />
+    <UserList users={usersList} />
   </div>;
 }
 
